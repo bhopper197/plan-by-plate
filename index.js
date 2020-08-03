ef8fcdbf13c0ee28d5efd19e40c9ff47be15c249
Brandon Hopper <brandonhop8765@outlook.com>
2020-08-02 22:30:52 -0400

    Successfully implemented Auto Correct feature.

--- index.js
+++ index.js
@@ -1,11 +1,64 @@
 'use strict';
 
 const apiKey = "a573f79888c882ab54b3f22d1b68c8cd";
-const url = "https://developers.zomato.com/api/v2.1/search"
+const url = "https://developers.zomato.com/api/v2.1/search";
 
-function renderElements(){};
+const searchInput = "search-term";
 
-function formatQueryString(){};
+$(document).ready(function inputAutoComplete() {
+  var autoComplete = new 
+  google.maps.places.Autocomplete((document.getElementById("search-term")), {
+    types: ['geocode']
+  });
+
+    google.maps.event.addListener(autoComplete, "place_changed", function() {
+        let near_place = autoComplete.getPlace();
+        document.getElementById("latitude-input").value = 
+        near_place.geometry.location.lat();
+
+        document.getElementById("longitude-input").value = 
+        near_place.geometry.location.lng();
+
+        // document.getElementById("latitude-view").innerHTML = 
+        // near_place.geometry.location.lat();
+
+        // document.getElementById("longitude-view").innerHTML = 
+        // near_place.geometry.location.lng();
+        console.log(near_place.geometry.location.lat());
+        console.log(near_place.geometry.location.lng());
+    });
+
+  });
+
+  $(document).on("change", "#"+searchInput, function() {
+    document.getElementById("latitude-input").value = "";
+    document.getElementById("longitude-input").value = "";
+  });
+
+  const restaurant = [
+    {
+      name: "responseJson.restaurant.name",
+      cuisines: "responseJson.restaurant.cuisines",
+      user_rating: 
+      "responseJson.restaurant.user_rating.aggregate_rating",
+      highlights: "restaurant.highlights"
+    },
+  ];
+
+function makeTileHtml(){
+  const restaurant = `<div class="wrapper-tile">
+  <div class="restaurant-form">
+      <div class="input-fields">
+          <h1>${restaurant.name}</h1>
+          <h3>${restaurant.user_rating}</h3>
+          <p>${restaurant.highlights}</p>
+          <button id="add-plate">+</button>
+      </div>
+  </div>
+</div>`
+};
+
+function renderElements(){}; 
 
 function updateDOM(){};
 
